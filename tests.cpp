b162282a8e08e40a05eaf30c72adc7df5a07ca21
TonyLiu <ltsplice@gmail.com>
2023-05-04 11:13:28 -0400

    fixed testing for print range

--- tests.cpp
+++ tests.cpp
@@ -4,8 +4,8 @@
 #include "doctest.h"
 
 TEST_CASE("Print Range"){ // A
-    CHECK(printRange(-2,10) == "-2 -1 0 1 2 3 4 5 6 7 8 9 10");
-    CHECK(printRange(10,15) == "10 11 12 13 14 15");
+    CHECK(printRange(-2,10) == "-2 -1 0 1 2 3 4 5 6 7 8 9 10 ");
+    CHECK(printRange(10,15) == "10 11 12 13 14 15 ");
 }
 
 TEST_CASE("sum Range"){ // B
